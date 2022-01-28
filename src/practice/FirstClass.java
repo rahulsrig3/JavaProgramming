c1ce8e2826081eab9395e42c4048a7cb3c6a7bb5
rahulsrig3 <rsrivastava918@gmail.com>
2022-01-28 13:52:16 +0530

    Test case 2 automate

--- src/practice/FirstClass.java
+++ src/practice/FirstClass.java
@@ -5,6 +5,9 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("hello first practice in gitbash");
+		
+		System.out.println("hello first 2 practice in gitbash");
+		
 
 	}
 
---
+++ src/practice/SecondClass.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello second practice in gitbash");
+
+	}
+
+}
